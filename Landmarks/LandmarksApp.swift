9dcd9448490a4b593876d5569da883069e4f2157
Daniel Casado <daniel.casado@frogdesign.com>
2023-06-02 09:58:42 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by JOSE DANIEL CASADO on 02/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
